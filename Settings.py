2dfbb62b1f5e320665e8c64b954d964689208702
reinar-kajava <reinar.kajava@khk.ee>
2021-04-11 18:11:19 +0100

    Updated the game to enable the user to fire bullets using spacebar

--- Settings.py
+++ Settings.py
@@ -8,3 +8,9 @@ class Settings:
         self.screen_height = 600
         self.bg_color = (100,200,255)
         self.ship_speed_factor = 1
+
+        self.bullet_speed_factor = 0.5
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 64, 64, 64
+        self.bullets_allowed = 3
--- game.py
+++ game.py
@@ -1,22 +1,23 @@
 import pygame
+from pygame.sprite import Group
 
 from Settings import Settings
 from ship import Ship
 import functions as gf
 
 def run_game():
-    # init game and create display object
     pygame.init()
     game_settings = Settings()
     screen = pygame.display.set_mode((game_settings.screen_width, game_settings.screen_height))
     pygame.display.set_caption("Example Game")
 
-    # create ship
     ship = Ship(game_settings, screen)
+    bullets = Group()
 
     while True:
-        gf.check_events(ship)
+        gf.check_events(game_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(game_settings, screen, ship)
+        gf.update_bullets(bullets)
+        gf.update_screen(game_settings, screen, ship, bullets)
 # test game
 run_game()
