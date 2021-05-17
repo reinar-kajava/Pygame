359d0234f1074eb201852303c7ff3b1e5e2a0450
reinar-kajava <reinar.kajava@khk.ee>
2021-05-17 22:16:12 +0100

    Updated the scoreboard to update

--- functions.py
+++ functions.py
@@ -64,16 +64,19 @@ def update_screen(game_settings, screen, stats, sb, ship, aliens, bullets, playb
 
 
 """Bullets"""
-def update_bullets(game_settings, screen, ship, aliens, bullets):
+def update_bullets(game_settings, screen, stats, sb, ship, aliens, bullets):
     """Update bullets position and  remove old bullets"""
     bullets.update()
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
-    check_bullet_alien_collisions(game_settings, screen, ship, aliens, bullets)
+    check_bullet_alien_collisions(game_settings, screen, stats, sb, ship, aliens, bullets)
 
-def check_bullet_alien_collisions(game_settings, screen, ship, aliens, bullets):
+def check_bullet_alien_collisions(game_settings, screen, stats, sb, ship, aliens, bullets):
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
+    if collisions:
+        stats.score += game_settings.alien_points
+        sb.prepare_score()
     # Remove bullets and create new fleet
     if len(aliens) == 0:
         bullets.empty()
--- game.py
+++ game.py
@@ -35,7 +35,7 @@ def run_game():
         gf.check_events(game_settings, screen, ship, bullets, stats, playbutton)
         if stats.game_active == True:
             ship.update()
-            gf.update_bullets(game_settings, screen, ship, aliens,bullets)
+            gf.update_bullets(game_settings, screen, stats, sb, ship, aliens, bullets)
             gf.update_aliens(game_settings, stats, screen, ship, aliens, bullets)
         gf.update_screen(game_settings, screen, stats, sb, ship, aliens, bullets, playbutton)
 
